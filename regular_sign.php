3721e860e1c070f03c507bcc3bcae6b873b44a9f
jal-tw <jal@jal.tw>
2015-01-16 01:40:43 +0800

    Create regular_sign.php

---
+++ regular_sign.php
@@ -0,0 +1,32 @@
+<?php
+$zone_name = "jal.tw";
+$zone_file = "/usr/local/etc/namedb/master/jal.tw";
+$KSK_file = "/usr/local/etc/namedb/key/Kjal.tw.+008+33567.key";
+$ZSK_file = "/usr/local/etc/namedb/key/Kjal.tw.+008+42644.key";
+
+/* Your SOA in zone file must be or like follow format.
+@       IN      SOA     jal.tw. jal.jal.tw.     (
+                        2015011202      ;serial
+                        7200            ;refresh
+                        600             ;retry
+                        1209600         ;expire
+                        2400            ;NegativeCaching
+                        )
+*/
+
+$serial = exec ("/usr/bin/grep serial $zone_file | /usr/bin/awk '{print $1}'");
+
+// Use nslookup to get online soa record.
+//$serial = exec ("nslookup -type=soa jal.tw | grep serial | awk '{print $3}'");
+
+if (empty($serial)){
+  echo "Zone file SOA serial not found\n";
+}else {
+  $new_serial = $serial + 1;
+  $cmd="/usr/bin/sed -i '' -e 's/$serial/$new_serial/g' $zone_file";
+  exec($cmd);
+  echo "$cmd\nserial\t". gettype($new_serial) . "\t$new_serial" . "\n";
+  system("/usr/local/sbin/dnssec-signzone -r /dev/random -o $zone_name -k $KSK_file $zone_file $ZSK_file");
+  system("/usr/local/etc/rc.d/named reload");
+}
+?>
